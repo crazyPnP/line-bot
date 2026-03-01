50d6e5a84feada5069df42cc92a86bdffcc9efc4
liliankuo <liliankuo@linkence.com>
2026-03-01 22:51:50 +0800

    add requirements.txt

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# ====== LINE settings ======
+LINE_CHANNEL_ACCESS_TOKEN = "5CDlzXgX+t0PGtZZDD9UNJzXosCRLYFkrD/eLT6Y8eHoM1GyE86+h+ghcPKv90TKfzhlF5f6oGZB5U65Ds+aLYwHmpuUvi5Vo5AWClhpw58ISDYX1qLuczJ4o6s+rKHLMjau+nycuFzbKdb0fUsuiwdB04t89/1O/w1cDnyilFU="
+LINE_CHANNEL_SECRET = "a0648ba56340347c4e1a976bd7b22f51"
+
+# ====== Supabase settings ======
+SUPABASE_URL = "https://abxtrqidbprklqyqavmq.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFieHRycWlkYnBya2xxeXFhdm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NDIwMDksImV4cCI6MjA4MDQxODAwOX0.h3Mp9zjIB3QTJ5eKkTZFC8TKKQo3dNKBEk9MBinaQ5Y"
+SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFieHRycWlkYnBya2xxeXFhdm1xIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDg0MjAwOSwiZXhwIjoyMDgwNDE4MDA5fQ.4o-SlAGK0fYvZE1hE4wOdkGloyB-AXKBE9ewv4n6GDM"
+
+# Put teacher's user id for test
+TEACHER_USER_ID = "Ud70bb839d1b68dea4ddb9b84cb7ace6c"
+
+TIMEZONE = "Asia/Taipei"
+
+# 修正：加上字串引號
+RICH_MENU_STUDENT_ID = "richmenu-ba0d378dc428a7415edfdbc5cc2497fc"
+RICH_MENU_TEACHER_ID = "richmenu-b30c4b5d9366f30e00cc9e30bd895e0e"
+RICH_MENU_ADMIN_ID = "richmenu-7bc31bca66011851a9661b42648ab7b0"
